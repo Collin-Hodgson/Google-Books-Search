ae3fc19c31ebfd9f4bb914ecc289c49461dfee55
Collin <collinhodgs@gmail.com>
2020-10-29 00:55:44 -0600

    Changed imports

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -5,7 +5,7 @@ import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
+import { Input, FormBtn } from "../components/Form";
 
 function Books() {
   // Setting our component's initial state
