cf29e97bd3fa55da409c41c22e6afb798f8fe0e5
Collin <collinhodgs@gmail.com>
2020-10-29 01:34:28 -0600

    Deleted unused imports

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
-import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 import SaveBtn from "../components/SaveBtn";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, FormBtn } from "../components/Form";
