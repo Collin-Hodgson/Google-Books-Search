712cf29560044cbf1f45f47c0f0b60b8eec4c9a8
Collin <collinhodgs@gmail.com>
2020-10-29 03:01:33 -0600

    Added deleteBook function

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -7,6 +7,12 @@ import API from "../utils/API";
 function Detail(props) {
   const [book, setBook] = useState({});
 
+  function deleteBook(id) {
+    API.deleteBook(id)
+      .then((res) => loadBooks())
+      .catch((err) => console.log(err));
+  }
+
   // When this component mounts, grab the book with the _id of props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
   const { id } = useParams();
