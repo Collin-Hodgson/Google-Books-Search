319185e2b2d7d8b0da19c04a4c52d6cfff41650a
Collin <collinhodgs@gmail.com>
2020-10-29 02:16:34 -0600

    Updated app.js with correct imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
+import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
@@ -11,8 +11,8 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
+          <Route exact path={["/", "/search"]}>
+            <Search />
           </Route>
           <Route exact path="/books/:id">
             <Detail />
