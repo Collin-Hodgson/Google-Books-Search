57f79354f38c012e0fdfa3ed2ed37c0d88c18e6c
Collin <collinhodgs@gmail.com>
2020-10-29 01:33:23 -0600

    Changed export name from books to search

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -8,7 +8,7 @@ import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, FormBtn } from "../components/Form";
 
-function Books() {
+function Search() {
   // Setting our component's initial state
   const [books, setBooks] = useState([]);
   const [formObject, setFormObject] = useState({});
@@ -101,4 +101,4 @@ function Books() {
   );
 }
 
-export default Books;
+export default Search;
