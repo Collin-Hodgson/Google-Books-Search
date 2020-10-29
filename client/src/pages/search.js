aaec7d86e1e710cf8e461665516e24e7b1a357fd
Collin <collinhodgs@gmail.com>
2020-10-29 01:29:50 -0600

    Changing format

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
+import SaveBtn from "../components/SaveBtn";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
@@ -75,12 +76,10 @@ function Books() {
                 {books.map((book) => (
                   <ListItem key={book._id}>
                     <Container fluid>
-                      <Link to={"/books/" + book._id}>
-                        <strong>
-                          {book.title} by {book.author}
-                        </strong>
-                      </Link>
-                      <DeleteBtn onClick={() => deleteBook(book._id)} />
+                      <SaveBtn
+                        handleSave={handleSave}
+                        bookData={bookData}
+                      ></SaveBtn>
                     </Container>
                   </ListItem>
                 ))}
