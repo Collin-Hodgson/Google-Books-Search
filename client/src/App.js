140fb40c54468656967c615c3db0c3efb0a4f536
Collin <collinhodgs@gmail.com>
2020-11-02 01:42:40 -0700

    Pulled files from class repo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Search from "./pages/Search";
-import Saved from "./pages/Saved";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -11,11 +11,11 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path={["/", "/search"]}>
-            <Search />
+          <Route exact path={["/", "/books"]}>
+            <Books />
           </Route>
           <Route exact path="/books/:id">
-            <Saved />
+            <Detail />
           </Route>
           <Route>
             <NoMatch />
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,33 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <h1>Google Books Search</h1>
-      <button
-        class="navbar-toggler hidden-lg-up"
-        type="button"
-        data-toggle="collapse"
-        data-target="#collapsibleNavId"
-        aria-controls="collapsibleNavId"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      ></button>
-      <div class="collapse navbar-collapse" id="collapsibleNavId">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item active">
-            <Link class="nav-link" to="/">
-              Search
-            </Link>
-          </li>
-          <li class="nav-item">
-            <Link class="nav-link" to="/saved">
-              Saved
-            </Link>
-          </li>
-        </ul>
-      </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        React Reading List
+      </a>
     </nav>
   );
 }
